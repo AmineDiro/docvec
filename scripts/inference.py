correct average pooling

scripts/inference.py
@@ -8,12 +8,14 @@ def average_pool(last_hidden_states: Tensor, attention_mask: Tensor) -> Tensor:
     return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
 
 
-input_texts = [
-    "what is the capital of China?",
-]
+def read_input():
+    with open("./data/index_content.txt", "r") as f:
+        return f.read().splitlines()[:10]
 
-tokenizer = AutoTokenizer.from_pretrained("./gte-small")
-model = AutoModel.from_pretrained("./gte-small")
+
+input_texts = read_input()
+tokenizer = AutoTokenizer.from_pretrained("./model/gte-small")
+model = AutoModel.from_pretrained("./model/gte-small")
 
 # Tokenize the input texts
 batch_dict = tokenizer(
@@ -25,8 +27,13 @@ batch_dict = tokenizer(
 )
 
 outputs = model(**batch_dict)
+embeddings = average_pool(outputs.last_hidden_state, batch_dict["attention_mask"])
+data = embeddings.flatten().tolist()
+import struct
 
-# embeddings = average_pool(outputs.last_hidden_state, batch_dict["attention_mask"])
+buffer = struct.pack(f"<{len(data)}f", *data)
+with open("../data/index_embedding.bin", "wb") as f:
+    f.write(buffer)
 
 # (Optionally) normalize embeddings
 # embeddings = F.normalize(embeddings, p=2, dim=1)

scripts/test_onnx.py
@@ -1,12 +1,23 @@
 import onnxruntime as ort
-from transformers import AutoModel, AutoTokenizer
+import torch
+from transformers import AutoTokenizer
 
-workdir = "/Users/aminedirhoussi/Documents/coding/doc-wasm/model/all-MiniLM-L6-v2"
-# model_path = workdir+'/model-prepared.onnx'
-model_path = workdir + "/sim_model.onnx"
-sentences = ["This is an example sentence"]
+workdir = "/Users/aminedirhoussi/Documents/coding/doc-wasm/model/gte-small"
+model_path = workdir + "/onnx/sim_model.onnx"
 
 tokenizer = AutoTokenizer.from_pretrained(workdir)
+
+
+def average_pool(last_hidden_states, mask):
+    last_hidden = last_hidden_states.masked_fill(~mask[..., None].bool(), 0.0)
+    return last_hidden.sum(dim=1) / mask.sum(dim=1)[..., None]
+
+
+# loading content
+with open("./data/index_content.txt", "r") as f:
+    first = f.read().splitlines()[0]
+
+sentences = [first]
 
 encoded_input = tokenizer(
     sentences,
@@ -20,5 +31,7 @@ encoded_input = tokenizer(
 ort_sess = ort.InferenceSession(model_path, providers=["CPUExecutionProvider"])
 print(encoded_input.keys())
 outputs = ort_sess.run(None, dict(encoded_input))
-embd = outputs[1].flatten()
+last_hidden_states = torch.Tensor(outputs[0])  # (b, 512,384)
+mask = torch.Tensor(encoded_input["attention_mask"])
+embd = average_pool(last_hidden_states, mask).numpy()
 print("Output shape: ", embd.shape)